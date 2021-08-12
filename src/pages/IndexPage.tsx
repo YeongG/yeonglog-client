b2733c27a81ec0858e2e1e305373aa443a13c2de
yeongg <yeongg223@gmail.com>
2021-08-12 23:05:29 +0900

    create IndexPage

---
+++ src/pages/IndexPage.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import * as S from "./styles";
+
+interface Props {}
+
+const IndexPage: FC<Props> = () => {
+  return <S.Container></S.Container>;
+};
+
+export default IndexPage;
